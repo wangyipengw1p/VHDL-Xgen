2877c57f24ee9bbf4f330366fa64a0ced078f3c5
wangyipengw1p <1014625143@qq.com>
2019-05-28 02:18:12 +0800

    Update vxgen.py

--- src/vxgen.py
+++ src/vxgen.py
@@ -1,1 +1,26 @@
 # 2019.5.28 start
+import sys
+
+
+def main():
+	if len(sys.argv) < 1:
+		print("Usage: python vxgen.py <func> <args>")
+		exit(1)
+	Fun =  sys.argv[1]
+	if Fun == 'gen':
+		generation(sys.argv[2:])
+	else if Fun == 'add':
+		addComponents(sys.argv[2:])
+	else if Fun == 'top':
+		topGen(sys.argv[2:])
+	else if Fun == 'tb':
+		tbGen(sys.argv[2:])
+	else if Fun == 'pkg':
+		pgkGen(sys.argv[2:])
+	else if Fun == 'clear':
+		vxClear()
+	else:
+		printInfo()
+
+if __name__ == '__main__':
+    main()
