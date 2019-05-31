06a9b456d2799083ee4de0fdb771a99033f7ec72
wangyipengw1p <1014625143@qq.com>
2019-05-31 19:04:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 basepath=$(cd `dirname $0`; pwd) #get cwd
 echo  >> ~/.bashrc
 echo 'export VHDLXGEN_PATH='$basepath >> ~/.bashrc
-echo alias vxgen='python $VHDLXGEN_PATH/src/vxgen.py'  >> ~/.bashrc
+echo alias vxgen='python '$basepath'/src/vxgen.py'  >> ~/.bashrc
