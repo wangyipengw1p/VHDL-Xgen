d6794ad7709c6d3da91699cb2915688ad83a60f7
wangyipengw1p <1014625143@qq.com>
2019-05-31 19:07:12 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 basepath=$(cd `dirname $0`; pwd) #get cwd
-echo  >> ~/.bashrc
-echo 'export VHDLXGEN_PATH='$basepath >> ~/.bashrc
-echo alias vxgen='python '$basepath'/src/vxgen.py'  >> ~/.bashrc
+echo  >> ~/.bashrc      
+# You can also custom the following by yourself
+echo 'export VHDLXGEN_PATH='$basepath >> ~/.bashrc            # set the environment variable necessary for this tool
+echo alias vxgen='python '$basepath'/src/vxgen.py'  >> ~/.bashrc        # set alias
