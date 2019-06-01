f94c4efbd224d181caa475dcc35cb03089494502
wangyipengw1p <1014625143@qq.com>
2019-06-01 11:16:16 +0800

    Update setup_win.cmd

--- setup_win.cmd
+++ setup_win.cmd
@@ -1,2 +1,3 @@
 @echo off
+echo The following line makes "%~dp0\data\setup_win.cmd" run once whenever cmd is opend > nul
 reg add "HKLM\Software\Microsoft\Command Processor" /v "AutoRun" /t REG_SZ /d "%~dp0\data\setup_win.cmd" /f
